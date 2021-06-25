015f127d45debbf518fe460366dcade014da9608
lincowj <lincoln.souza@webjump.com.br>
2021-06-25 16:36:19 -0300

    modified files from previous lessons, as required on lesson 6

--- avancando-com-php/funcoes.php
+++ avancando-com-php/funcoes.php
@@ -30,3 +30,9 @@ function titularComLetrasMaiusculas(array &$conta)
 {
     $conta['titular'] = strtoupper($conta['titular']);
 }
+
+function exibeConta(array $conta)
+{
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li>Titular: $titular. Saldo: $saldo</li>";
+}
