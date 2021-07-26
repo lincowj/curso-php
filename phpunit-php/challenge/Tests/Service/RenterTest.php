1b100735ac7d3941feb431c6b41048dbf75531c7
lincowj <lincoln.souza@webjump.com.br>
2021-07-26 17:54:43 -0300

    Revert "implementing data providers"
    This reverts commit e36791446e3b5c756922e5a086566043bd7ea7f4.

--- phpunit-php/challenge/Tests/Service/RenterTest.php
+++ phpunit-php/challenge/Tests/Service/RenterTest.php
@@ -12,38 +12,40 @@ require_once './phpunit-php/challenge/vendor/autoload.php';
 
 class RenterTest extends TestCase
 {
-    /**
-     * @dataProvider carsAndCustomers
-     */
-    public function testDoNotRentToAClientWhoIsAlreadyWithACar(array $customers, array $cars)
+    public function testDoNotRentToAClientWhoIsAlreadyWithACar()
     {
         $this->expectException(DomainException::class);
         $this->expectExceptionMessage('This person is already renting a car');
         
+        $toro = new Car('Fiat', 'Toro', 'ABC1D23', 150,0);
+        $taos = new Car('Volkswagen', 'Taos', 'DEF4G56', 200,0);
+        $joao = new Customer('João', '123.456.789.10');
         $renter = new Renter();
 
-        $renter->rent($customers[0], $cars[0], '1');
-        $renter->rent($customers[0], $cars[1], '5');
+        $renter->rent($joao, $toro, '1');
+        $renter->rent($joao, $taos, '5');
     }
 
-    /**
-     * @dataProvider carsAndCustomers
-     */
-    public function testRentingTo3DifferentPeople(array $customers, array $cars)
+    public function testRentingTo3DifferentPeople()
     {
+        $toro = new Car('Fiat', 'Toro', 'ABC1D23', 150,0);
+        $taos = new Car('Volkswagen', 'Taos', 'DEF4G56', 200,0);
+        $taos2 = new Car('Volkswagen', 'Taos', 'KJS4P46', 200,0);
+
+        $joao = new Customer('João', '123.456.789.10');
+        $maria = new Customer('Maria', '456.789.123-50');
+        $ana = new Customer('Ana', '789.456.321-80');
+
         $renter = new Renter();
 
-        $renter->rent($customers[0], $cars[2], '9');
-        $renter->rent($customers[1], $cars[1], '5');
-        $renter->rent($customers[2], $cars[0], '8');
+        $renter->rent($joao, $taos, '9');
+        $renter->rent($ana, $taos2, '5');
+        $renter->rent($maria, $toro, '8');
 
         self::assertCount(3, $renter->getAllRentedCars());
     }
 
-    /**
-     * @dataProvider carsAndCustomers
-     */
-    public function testDoNotRentACarThatIsAlreadyRented(array $customers, array $cars)
+    public function testDoNotRentACarThatIsAlreadyRented()
     {
         $this->expectException(DomainException::class);
         $this->expectExceptionMessage('This car is already rented');
@@ -51,28 +53,14 @@ class RenterTest extends TestCase
         $toro = new Car('Fiat', 'Toro', 'ABC1D23', 150,0);
         $taos = new Car('Volkswagen', 'Taos', 'DEF4G56', 200,0);
 
-        $renter = new Renter();
-
-        $renter->rent($customers[2], $cars[1], '9');
-        $renter->rent($customers[0], $cars[2], '8');
-        $renter->rent($customers[1], $cars[1], '5');
-    }
-
-    public function carsAndCustomers()
-    {
-        $toro = new Car('Fiat', 'Toro', 'ABC1D23', 150,0);
-        $taos = new Car('Volkswagen', 'Taos', 'DEF4G56', 200,0);
-        $hilux = new Car('Toyota', 'Hilux', 'KJS4P46', 300,0);
-
         $joao = new Customer('João', '123.456.789.10');
         $maria = new Customer('Maria', '456.789.123-50');
         $ana = new Customer('Ana', '789.456.321-80');
 
-        return [
-            'data' => [
-                [$joao, $ana, $maria], 
-                [$toro, $taos, $hilux]
-            ]
-        ];
+        $renter = new Renter();
+
+        $renter->rent($joao, $taos, '9');
+        $renter->rent($maria, $toro, '8');
+        $renter->rent($ana, $taos, '5');
     }
 }
